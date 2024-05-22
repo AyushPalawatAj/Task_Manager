32f0e78d1c57a7ce9268b8ea868230c257654c76
Ayush Singh <ayushsingh.jat28@gmail.com>
2024-05-22 09:21:46 +0530

    server sid change

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,25 +1,22 @@
 const jwt = require("jsonwebtoken");
-// const config = require("../config"); // Assuming your secret key is stored in config.js
 
 const authenticateToken = (req, res, next) => {
-  // Extract token from Authorization header
+ 
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; // Remove "Bearer " prefix
+  const token = authHeader && authHeader.split(" ")[1]; 
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized: Missing access token" });
   }
 
-  // Verify token using secret key
   jwt.verify(token, "tomAYU" , (err, user) => {
     if (err) {
       return res.status(403).json({ message: "Forbidden: Invalid or expired token" });
     }
 
-    // Attach user data to request object
     req.user = user;
 
-    next(); // Proceed to next middleware
+    next(); 
   });
 };
 
